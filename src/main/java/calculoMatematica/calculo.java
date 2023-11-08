383007d1098b5b6dbe970733b6568d0ad9a6c8b1
martamarinho <martamarinhoo@hotmail.com>
2023-11-07 21:27:54 -0300

    subindocalculadora

---
+++ src/main/java/calculoMatematica/calculo.java
@@ -0,0 +1,23 @@
+package calculoMatematica;
+
+public class calculo {
+
+	public static void main(String[] args) {
+		
+		calculoMatematica ca = new calculoMatematica();
+
+			ca.divisao(34,34);
+			ca.divisao(45,5);
+			ca.divisao(81,9);
+			ca.multiplicacao(81,9);
+			ca.multiplicacao(54,4);
+			ca.multiplicacao(30,10);
+			ca.subtracao(40,20);
+			ca.subtracao(60,40);
+			ca.subtracao(43,16);
+			ca.soma(100,25);
+			ca.soma(300,124);
+			ca.soma(68,12);
+	}
+
+}
---
+++ src/main/java/calculoMatematica/calculoMatematica.java
@@ -0,0 +1,26 @@
+package calculoMatematica;
+
+public class calculoMatematica {
+	
+	
+	public void divisao (int n1 ,int n2) {
+	           int divisao= n1/n2;
+	           System.out.println(divisao);}
+	
+   public void multiplicacao (int n1 , int n2) {
+	   int multiplicacao= n1*n2;
+	   System.out.println(multiplicacao);}
+   
+   public void subtracao(int n1 , int n2) {
+	   int subtracao= n1-n2;
+	   System.out.println(subtracao);}
+   
+   public void soma (int n1 , int n2) {
+	   int soma= n1+n2;
+	   System.out.println(soma);}
+   }
+   
+   
+	
+
+
